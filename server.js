53338383a33a39acf2a80719e29f38c5d17c4b84
Abdulrachman Hasan <hasanabdulrachman@gmail.com>
2022-02-15 21:08:01 +0700

    Define requested routes with mock handler

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
 require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
 
 app.use(cors())
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
-});
+})
 
 
+app.post('/api/users', function (req, res) {
+  console.log('di post /api/users')
+})
 
+app.get('/api/users', function (req, res) {
+  console.log('di get /api/users')
+})
+
+app.post('/api/users/:_id/exercises', function (req, res) {
+  console.log('di post exercises')
+})
+
+app.get('/api/users/:_id/logs', function (req, res) {
+  console.log('di get exercises logs count')
+})
+
+app.get('/api/users/:id/logs', function (req, res) {
+  console.log('di get exercise logs array')
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
